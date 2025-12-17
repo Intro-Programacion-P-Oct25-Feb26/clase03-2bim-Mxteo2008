283d5c7d3fe25931281c0dcd28813bd4781d3412
Mxteo2008 <mateo.jimenez.bvm@gmail.com>
2025-12-17 12:31:07 -0500

    commit

--- Ejemplosc3/src/paquete03/Ejemplo01.java
+++ Ejemplosc3/src/paquete03/Ejemplo01.java
@@ -17,12 +17,14 @@ public class Ejemplo01 {
                             {7, 9.4, 10, 9}};
         String[] nombresEstudiante = {"Jose A", "Elena J", "Paúl L"};
         double[] sumaNotas = new double[3];
-        double[] sumaNotasCualitativas = new double[3];
+        String[] notasCualitativas = new String[3];
+        
         // 0 - 5.9 Bueno
         // 6 - 8.9 Muy Bueno
         // 9 - 10 Sobresaliente
                     
         double suma;
+        double[] promedio = new double[3];
         String reporte = "";
         
         for (int i = 0; i < notas.length; i++) { // 0<3
@@ -30,20 +32,42 @@ public class Ejemplo01 {
             for (int j = 0; j < notas[i].length; j++) { // 0<notas[0].length
                                                         // 0<4 // 1<4 // 2<4 // 3<4 // 4<4
                 suma = suma + notas[i][j];
+                
                      // 0 + notas[0][0] // 0 + 9 // 9
                      // 9 + notas[0][1] // 9 + 10 // 19
                      // 19 + notas[0][2] // 19 + 7 // 26
                      // 26 + notas[0][3] // 26 + 8 // 34
             }
-            sumaNotas[i] = suma; // 
+            sumaNotas[i] = suma;
+            
+            promedio[i] = suma / notas[i].length;
+            if(promedio[i] >= 0 && promedio[i] < 6){
+                notasCualitativas[i] = "Bueno";
+            }else{
+                if(promedio[i] >= 6 && promedio[i] < 9){
+                    notasCualitativas[i] = "Muy bueno";
+                }else{
+                    if(promedio[i] >= 9 && promedio[i] <= 10){
+                        notasCualitativas[i] = "Sobresaliente";
+                          
+                }else{
+                        notasCualitativas[i] = "fuera de rango";
+                    }
+                        
+                    }
+                
+            }
+        
         }
         // Presentación de resultados
         for (int i = 0; i < nombresEstudiante.length; i++) {
             reporte = String.format("%sEstudiante %s tiene una sumataria "
-                    + "de notas: %.3f\n",
+                    + "de notas: %.3f y su promedio: %.2f (%s)\n",
                     reporte,
                     nombresEstudiante[i],
-                    sumaNotas[i]);
+                    sumaNotas[i],
+                    promedio[i],
+                    notasCualitativas[i]);
         }
         
         System.out.printf("%s\n", reporte);
