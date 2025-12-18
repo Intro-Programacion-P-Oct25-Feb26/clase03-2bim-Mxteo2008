b92ef499ce355ca900efcc4eb528197baad0c3f2
Mxteo2008 <mateo.jimenez.bvm@gmail.com>
2025-12-17 22:49:58 -0500

    ejercicio paquete 5

--- Ejemplosc3/src/paquete05/Ejercicio.java
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -1,56 +1,60 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package paquete05;
 
 import java.util.Scanner;
+import java.util.Locale;
 
-/**
- *
- * @author UTPL
- */
 public class Ejercicio {
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
+        entrada.useLocale(Locale.US);
         String[] nombres = new String[5];
         double[][] produccion = new double[5][12];
         double[] sumaTotal = new double[5];
         String nombre;
-        String reporte = "";
-        double suma;
         double produccionValor;
+        String reporte = String.format("Análisis de Producción\n");
 
+        // Entrada de datos
         for (int i = 0; i < produccion.length; i++) {
-
-            System.out.println("Ingrese le nombre del encargado");
+            System.out.printf("Ingrese el nombre del encargado de la estación "
+                    + "%d:%n", i + 1);
             nombre = entrada.nextLine();
             nombres[i] = nombre;
-            System.out.println("Encargado " +nombre);
-            for (int j = 1; j <= produccion[i].length; j++) {
-                System.out.println("mes " + j);
-                System.out.println("Ingrese la producción del mes");
+
+            for (int j = 0; j < produccion[i].length; j++) {
+                System.out.printf("Mes %d - Ingrese la producción en "
+                        + "dólares:%n", j + 1);
                 produccionValor = entrada.nextDouble();
                 produccion[i][j] = produccionValor;
-
-                entrada.nextLine();
             }
-
+            entrada.nextLine();
         }
-        for (int i = 0; i < produccion.length; i++){
-            suma = 0;
-            for (int j = 0; j < produccion[i].length; j++){
+
+        double maximo = 0;
+        int indiceMaximo = 0;
+
+        for (int i = 0; i < produccion.length; i++) {
+            double suma = 0;
+            for (int j = 0; j < produccion[i].length; j++) {
                 suma = suma + produccion[i][j];
             }
-            sumaTotal[i] = suma;         
-        
-        }
-        
-        for (int i = 0; i < nombres.length; i++){
-            reporte = String.format("", )
-        }
+            sumaTotal[i] = suma;
 
+            reporte = String.format("%sEstacion %s - %s - Total Producción: "
+                    + "$%.2f\n", reporte, i +1, nombres[i], suma);
+
+            if (suma > maximo) {
+                maximo = suma;
+                indiceMaximo = i;
+            }
+        }
+        reporte = String.format("%s%nEstación más productiva: Estación "
+                + "%d%nEncargado "
+                + "de la estación: %s%nCantidad de la estación más productiva: "
+                + "$%.2f\n", reporte, indiceMaximo + 1, nombres[indiceMaximo],
+                maximo);
+
+        System.out.println(reporte);
     }
-
 }
